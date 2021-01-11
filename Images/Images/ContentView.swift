2978eaa012f6c706834c375944e76c973ae30df4
FreddyQuispe <fquispe@smartsolutions.com.pe>
2021-01-11 14:32:47 -0500

    App 2 terminada

--- Images/Images/ContentView.swift
+++ Images/Images/ContentView.swift
@@ -9,31 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Image(systemName: "recordingtape")
-//            .font(.system(size: 80))
-//            .foregroundColor(.green)
-//            .shadow(color: .gray, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 0, y: 10)
-        HStack (spacing: 0) {
-            Image(systemName: "f.square")
-                .font(.system(size: 40))
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            Image(systemName: "r.circle.fill")
-                .font(.system(size: 40))
-                .foregroundColor(.green)
-            Image(systemName: "e.circle")
-                .font(.system(size: 40))
-                .foregroundColor(.red)
-            Image(systemName: "d.circle.fill")
-                .font(.system(size: 40))
-                .foregroundColor(.purple)
-            Image(systemName: "d.square")
-                .font(.system(size: 40))
-                .foregroundColor(.orange)
-            Image(systemName: "y.circle.fill")
-                .font(.system(size: 40))
-                .foregroundColor(.gray)
-        }
-        
+        Image("newyork")
+            .resizable()
+//            .edgesIgnoringSafeArea(.vertical)
+            //.scaledToFit()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 300)
+            //.clipped()
+            .clipShape(Capsule())
+            .opacity(0.9)
+            .overlay(
+                /*Image(systemName: "heart.fill")
+                    .font(.system(size: 60))
+                    .foregroundColor(.pink)
+                    .opacity(0.5)*/
+                /*Text("Que ganas de volver a Nueva York, sentir el frio de invierno y pasear por el Central Park con un buen Bagel en la mano!")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(.system(.headline, design: .rounded))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .opacity(0.7)
+                    .padding(),
+                alignment: .bottom*/
+                Capsule()
+                    .foregroundColor(.gray)
+                    .opacity(0.5)
+                    .overlay(
+                        Text("Nueva York")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(width:250)
+                    )
+            )
     }
 }
 
@@ -42,3 +52,24 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//        HStack (spacing: 0) {
+//            Image(systemName: "f.square")
+//                .font(.system(size: 40))
+//                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+//            Image(systemName: "r.circle.fill")
+//                .font(.system(size: 40))
+//                .foregroundColor(.green)
+//            Image(systemName: "e.circle")
+//                .font(.system(size: 40))
+//                .foregroundColor(.red)
+//            Image(systemName: "d.circle.fill")
+//                .font(.system(size: 40))
+//                .foregroundColor(.purple)
+//            Image(systemName: "d.square")
+//                .font(.system(size: 40))
+//                .foregroundColor(.orange)
+//            Image(systemName: "y.circle.fill")
+//                .font(.system(size: 40))
+//                .foregroundColor(.gray)
+//        }
