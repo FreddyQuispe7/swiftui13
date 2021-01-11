a38ebb9c7c173593f308b3b8024dbf6431d9fb0e
FreddyQuispe <fquispe@smartsolutions.com.pe>
2021-01-11 12:39:51 -0500

    App1 completada

--- FirstApp/FirstApp/ContentView.swift
+++ FirstApp/FirstApp/ContentView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hola mundo")
-            .fontWeight(.bold)
-            .font(.system(size: 45, design: .rounded))
+        Text("Soy Tecnico Electronico, especialista en Automatizacion y Control Industrial, egresado de Tecsup, con estudios de ingenieria electronica en la Pontificia Universidad Catolica del Peru. Actualmente me encuentro desarrollando mi nuevo perfil profesional, estudiando Ingenieria de Sistemas en la UPC.")
+            .fontWeight(.regular)
+            .font(.custom("Gill Sans", size: 25))
             .foregroundColor(.blue)
+            .multilineTextAlignment(.center)
+            .lineLimit(nil)
+            .truncationMode(.tail)
+            .lineSpacing(8)
+            .padding()
+            //.rotationEffect(.degrees(15), anchor: UnitPoint(x: 0, y: 0))
+            .rotation3DEffect(.degrees(30), axis: (x:1, y:0, z:0))
+            .shadow(color: .gray, radius: 1, x: 0, y: 5)
     }
 }
 
