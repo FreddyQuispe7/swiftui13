764f6daf8e8eb25f9337e259d89b18b3c9b4b5c6
FreddyQuispe <fquispe@smartsolutions.com.pe>
2021-01-11 12:02:28 -0500

    Proyecto de prueba

---
+++ prueba/prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prueba
+//
+//  Created by Freddy Alexander Quispe Torres on 11/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
