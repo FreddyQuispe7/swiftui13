ba2fc46da2fbdd20a088a6525b4338a8bda71853
FreddyQuispe <fquispe@smartsolutions.com.pe>
2021-01-12 19:10:33 -0500

    Ejercicio autonomo de Cards

---
+++ ScrollCardView/ScrollCardView/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  ScrollCardView
+//
+//  Created by Freddy Alexander Quispe Torres on 12/01/21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
