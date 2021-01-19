71d9714f4b8583c74900aaa4b4fbaba48c8ec0b9
FreddyQuispe <fquispe@smartsolutions.com.pe>
2021-01-18 19:13:49 -0500

    App 11 avance

---
+++ Navigation/Navigation/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Freddy Alexander Quispe Torres on 18/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var courses = [
+        Course(name: "Probabilidad para Machine Learning y Big Data con R y Python", image: "probabilidades"),
+        Course(name: "Machine Learning de A a la Z: R y Python para Data Science", image: "machine_learning", feature: true),
+        Course(name: "Experto en Firebase para Android + MVP Curso Completo +30hrs", image: "android_firebase"),
+        Course(name: "POO en C#: aplica conceptos de POO en C# en proyectos reales", image: "csharp", feature: true),
+        Course(name: "Curso de Android con Kotlin: Intensivo y práctico desde cero", image: "kotlin"),
+        Course(name: "Aplica SQL al mundo del Data Science desde cero", image: "sql", feature: true),
+        Course(name: "Diseño de apps para iOS 13 con Swift UI desde cero", image: "swift"),
+        Course(name: "Curso completo de iOS 13 con Swift UI 5.2: de cero a experto", image: "swift2", feature: true),
+        Course(name: "Curso completo de Unity 2020: domina el mundo de videojuegos", image: "unity")
+        ]
+    
+    var body: some View {
+        List(courses.indices) { index in
+            if courses[index].feature {
+                CourseFullImageRow(course: self.courses[index])
+            } else {
+                CourseRoundImageRow(course: self.courses[index])
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Course: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+    var feature: Bool = false
+}
+
+struct CourseRoundImageRow: View {
+    var course: Course
+    
+    var body: some View {
+        HStack {
+            Image(course.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 60, height: 60)
+                .clipped()
+                .cornerRadius(30)
+            Text(course.name)
+        }
+    }
+}
+
+struct CourseFullImageRow: View {
+    var course: Course
+    
+    var body: some View {
+        ZStack {
+            Image(course.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 200)
+                .cornerRadius(15)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundColor(.gray)
+                        .opacity(0.25)
+                )
+            Text(course.name)
+                .font(.system(.headline, design: .rounded))
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+        }
+    }
+}
---
+++ Navigation/Navigation/PilotosView.swift
@@ -0,0 +1,80 @@
+//
+//  PilotosView.swift
+//  Lists
+//
+//  Created by Freddy Alexander Quispe Torres on 18/01/21.
+//
+
+import SwiftUI
+
+let mercedes = Equipo(color: Color(red: 79.0/255.0, green: 164.0/255.0, blue: 161.0/255.0), imagen: "car_mercedes")
+let ferrari = Equipo(color: Color(red: 231.0/255.0, green: 50.0/255.0, blue: 40.0/255.0), imagen: "car_ferrari")
+let redbull = Equipo(color: Color(red: 34.0/255.0, green: 44.0/255.0, blue: 54.0/255.0), imagen: "car_redbull")
+let renault = Equipo(color: Color(red: 240.0/255.0, green: 238.0/255.0, blue: 95.0/255.0), imagen: "car_renault")
+let mclaren = Equipo(color: Color(red: 240.0/255.0, green: 172.0/255.0, blue: 61.0/255.0), imagen: "car_mclaren")
+
+struct PilotosView: View {
+
+    var pilotos = [
+        Piloto(nombre: "Lewis Hamilton", imagen: "hamilton", equipo: mercedes),
+        Piloto(nombre: "Valteri Bottas", imagen: "bottas", equipo: mercedes),
+        Piloto(nombre: "Sebastian Vettel", imagen: "vettel", equipo: ferrari),
+        Piloto(nombre: "Charles Leclerc", imagen: "leclerc", equipo: ferrari),
+        Piloto(nombre: "Max Verstappen", imagen: "verstappen", equipo: redbull),
+        Piloto(nombre: "Pierre Gasly", imagen: "gasly", equipo: redbull),
+        Piloto(nombre: "Daniel Ricciardo", imagen: "ricciardo", equipo: renault),
+        Piloto(nombre: "Andre Hulkenberg", imagen: "hulkenberg", equipo: renault),
+        Piloto(nombre: "Carlos Sainz", imagen: "sainz", equipo: mclaren),
+        Piloto(nombre: "Julen Norris", imagen: "norris", equipo: mclaren)
+        ]
+    
+    
+    var body: some View {
+        List(pilotos){ piloto in
+            PilotosListaRow(piloto: piloto)
+            
+        }
+    }
+}
+
+struct PilotosView_Previews: PreviewProvider {
+    static var previews: some View {
+        PilotosView()
+    }
+}
+
+
+struct Piloto: Identifiable {
+    var id = UUID()
+    var nombre: String
+    var imagen: String
+    //var equipo: String
+    var equipo: Equipo
+}
+
+struct Equipo {
+    var color: Color
+    var imagen: String
+}
+
+struct PilotosListaRow: View {
+    var piloto: Piloto
+    
+    var body: some View {
+        HStack {
+            Image(piloto.imagen)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 80, height: 80)
+                .clipped()
+                .background(piloto.equipo.color)
+                .cornerRadius(40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .foregroundColor(.gray)
+                        .opacity(0.2)
+                )
+            Text(piloto.nombre)
+        }
+    }
+}
